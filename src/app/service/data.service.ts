94c0e5afb412c746bea27644e2c87e557c46dd98
dayveedo1 <dayveedo1@gmail.com>
2020-10-17 23:14:44 +0100

    update on dataservice

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -8,7 +8,7 @@ import { GlobalDataSummary } from 'src/app/model/global-data';
 })
 export class DataService {
 
-  private globalDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/10-14-2020.csv';
+  private globalDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/10-16-2020.csv';
 
   constructor(private http: HttpClient) { }
 
